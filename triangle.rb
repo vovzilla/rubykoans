5a821467578be404f31a633ff2bbc09dc9b1572a
Omar <vovzilla@gmail.com>
2012-07-30 16:54:34 -0500

    triangleError
    /

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
-end
-
-# Error class used in part 2.  No need to change this code.
-class TriangleError < StandardError
+    if a == b and b == c
+        :equilateral
+        elsif (a == b) or (a == c) or (b == c)
+        :isosceles
+        else
+        :scalene 
+	end
+    # Error class used in part 2.  No need to change this code.
+    class TriangleError < StandardError
 end
